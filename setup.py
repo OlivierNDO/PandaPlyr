a7f433bdd80a6cd5f5a95c738478e98097c37fde
OlivierNDO <Olivier_N@lynchburg.edu>
2023-07-19 11:39:50 -0500

    changing name in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PythonPlyr",
+    name="PandaPlyr",
     version="0.0.1",
     author="Nick Olivier",
     author_email="Olivier_N@lynchburg.edu",
-    description="The purpose of PyPlyr is to make chained operations on pandas DataFrames easier and more readable.",
+    description="The purpose of PandaPlyr is to make chained operations on pandas DataFrames easier and more readable.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/OlivierNDO/PythonPlyr/",
+    url="https://github.com/OlivierNDO/PandaPlyr/",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
