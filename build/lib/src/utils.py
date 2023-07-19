25323949a2a6075b6b7d0e25d9b9dee50ba71008
OlivierNDO <Olivier_N@lynchburg.edu>
2023-07-19 10:57:09 -0500

    adding gitignore, setup.py

---
+++ build/lib/src/utils.py
@@ -0,0 +1,47 @@
+### Configuration
+###############################################################################
+# Import packages
+import os
+import pandas as pd
+
+
+### Define Classes & Functions
+###############################################################################
+def read_titanic_dataset():
+    """
+    Read the Titanic dataset from the CSV file into a pandas Dataframe
+
+    Returns:
+    -------
+    pandas.DataFrame:
+        The loaded Titanic dataset.
+    """
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = current_file_path.split('PyPlyr')[0] + 'PyPlyr/data/titanic.csv'
+    return pd.read_csv(file_path)
+
+def read_grades_dataset():
+    """
+    Read the Titanic dataset from the CSV file into a pandas Dataframe
+
+    Returns:
+    -------
+    pandas.DataFrame:
+        The loaded Titanic dataset.
+    """
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = current_file_path.split('PyPlyr')[0] + 'PyPlyr/data/student_grades.csv'
+    return pd.read_csv(file_path)
+
+def read_subject_dataset():
+    """
+    Read the Titanic dataset from the CSV file into a pandas Dataframe
+
+    Returns:
+    -------
+    pandas.DataFrame:
+        The loaded Titanic dataset.
+    """
+    current_file_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = current_file_path.split('PyPlyr')[0] + 'PyPlyr/data/subject_categories.csv'
+    return pd.read_csv(file_path)
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pythonplyr",
+    version="0.0.1",
+    author="Nick Olivier",
+    author_email="Olivier_N@lynchburg.edu",
+    description="The purpose of PyPlyr is to make chained operations on pandas DataFrames easier and more readable.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OlivierNDO/PyPlyr/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'os',
+        'pandas',
+        're'
+    ],
+    python_requires='>=3.6',
+)
