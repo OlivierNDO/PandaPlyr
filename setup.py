6df0675c9503d0540b4e1a0e7344d3961e1b7c04
OlivierNDO <Olivier_N@lynchburg.edu>
2023-07-19 13:50:23 -0500

    updating setup structure

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="PandaPlyr",
-    version="0.0.6",
+    version="0.0.8",
     author="Nick Olivier",
     author_email="Olivier_N@lynchburg.edu",
     description="The purpose of PandaPlyr is to make chained operations on pandas DataFrames easier and more readable.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/OlivierNDO/PandaPlyr/",
-    packages=find_packages(),
+    packages=find_packages(where='src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# Import statements
+from .pandaplyr import *
+
+# Variables
+version = "0.0.7"
+author = "Nick Olivier"
+
+# Function
+def greet():
+    print("Welcome to PandaPlyr!")
